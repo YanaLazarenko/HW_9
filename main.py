a88298c0d67498c197c1862e87a5677a1dcfc5c2
YanaLazarenko <131553712+YanaLazarenko@users.noreply.github.com>
2023-07-30 16:12:01 +0300

    HW_9

---
+++ main.py
@@ -0,0 +1,91 @@
+phone_book = {}
+def input_error(func):
+    def inner_func(*args):
+        try:
+            return func(*args)
+        except IndexError:
+            return 'Please enter command, name, phone number'
+        except ValueError:
+            return 'Pleease enter correct number'
+        except KeyError:
+            return 'Name is not in phone book'
+        except TypeError:
+            return 'Unknown comand'
+    return inner_func
+
+
+@input_error
+def change_phone(*args):
+    for key in phone_book.keys():
+        if args[0] == key:
+            phone_book[key] = args[1]
+            return key, phone_book[key]
+        else: 
+            raise KeyError
+    
+
+@input_error
+def whose_phone(*args):
+    for key, value in phone_book.items():
+        if args[0] == key:
+            return value 
+        else:
+            raise KeyError 
+
+@input_error
+def add(*args):
+    name = args[0]
+    phone = args[1]
+    if name and phone:
+        phone_book[name] = phone
+        return f'{name} successfuly added'
+
+
+def show_all(*args):
+    return phone_book
+
+
+def hello(*args):
+    return 'How can I help you?'
+
+
+def no_comand():
+    return 'Please enter your comand'
+
+
+COMMANDS = {
+            'hello': hello,
+            'add': add,
+            'change': change_phone,
+            'phone': whose_phone,
+            'show all': show_all,
+            'no comands': no_comand,
+            }
+
+COMMANDS_EXIT = ['exit', 'quit', 'good bye']
+
+
+def parser(text: str) -> tuple[callable, list]:
+    for comand in COMMANDS.keys():
+        if text.lower().startswith(comand):
+            args = text[len(comand):].strip().split(' ')
+            return comand, args
+                
+
+def main():
+    while True:
+        user_input = input(f'Please enter comand:')
+        if not user_input:
+            no_comand()
+        elif user_input.lower() in COMMANDS_EXIT:
+            break
+        # elif user_input.lower() not in COMMANDS.keys():
+        #     print(f'{user_input} is not a command')
+        else:
+            comand, args = parser(user_input)
+            result = COMMANDS[comand](*args)
+            print(result)
+
+
+if __name__ == '__main__':
+    main()
